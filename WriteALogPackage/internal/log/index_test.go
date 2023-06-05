cc32331dd2fce37ff2d06854c120361b8196dcde
ita-jp <28795978+ita-jp@users.noreply.github.com>
2023-06-05 23:18:59 +0900

    Add test.

--- WriteALogPackage/internal/log/index_test.go
+++ WriteALogPackage/internal/log/index_test.go
@@ -57,3 +57,122 @@ func TestIndex(t *testing.T) {
 	require.Equal(t, uint32(1), off)
 	require.Equal(t, entries[1].Pos, pos)
 }
+
+func TestIndex_returnEOFWhenIndexIsEmpty(t *testing.T) {
+	f, err := os.CreateTemp(os.TempDir(), "index_test")
+	require.NoError(t, err)
+	defer func() {
+		if err := os.Remove(f.Name()); err != nil {
+			t.Errorf("got error = %v", err)
+		}
+	}()
+
+	c := Config{}
+	c.Segment.MaxIndexBytes = 1024
+
+	idx, err := newIndex(f, c)
+	require.NoError(t, err)
+
+	out, pos, err := idx.Read(0)
+	require.Equal(t, uint32(0), out)
+	require.Equal(t, uint64(0), pos)
+	require.Equal(t, io.EOF, err)
+}
+
+func TestIndex_returnPositionWhenIndexHasEntriesAndArgumentIsMinusOne(t *testing.T) {
+	f, err := os.CreateTemp(os.TempDir(), "index_test")
+	require.NoError(t, err)
+	defer func() {
+		if err := os.Remove(f.Name()); err != nil {
+			t.Errorf("got error = %v", err)
+		}
+	}()
+
+	c := Config{}
+	c.Segment.MaxIndexBytes = 1024
+
+	idx, err := newIndex(f, c)
+	require.NoError(t, err)
+
+	err = idx.Write(uint32(0), uint64(2))
+	require.NoError(t, err)
+
+	out, pos, err := idx.Read(-1)
+	require.Equal(t, uint32(0), out)
+	require.Equal(t, uint64(2), pos)
+	require.NoError(t, err)
+}
+
+func TestIndex_returnPositionWhenIndexHasEntriesAndArgumentIsZero(t *testing.T) {
+	f, err := os.CreateTemp(os.TempDir(), "index_test")
+	require.NoError(t, err)
+	defer func() {
+		if err := os.Remove(f.Name()); err != nil {
+			t.Errorf("got error = %v", err)
+		}
+	}()
+
+	c := Config{}
+	c.Segment.MaxIndexBytes = 1024
+
+	idx, err := newIndex(f, c)
+	require.NoError(t, err)
+
+	err = idx.Write(uint32(0), uint64(2))
+	require.NoError(t, err)
+
+	out, pos, err := idx.Read(0)
+	require.Equal(t, uint32(0), out)
+	require.Equal(t, uint64(2), pos)
+	require.NoError(t, err)
+}
+
+func TestIndex_returnEOFWhenGivenOffsetIsOutOfRange(t *testing.T) {
+	f, err := os.CreateTemp(os.TempDir(), "index_test")
+	require.NoError(t, err)
+	defer func() {
+		if err := os.Remove(f.Name()); err != nil {
+			t.Errorf("got error = %v", err)
+		}
+	}()
+
+	c := Config{}
+	c.Segment.MaxIndexBytes = 1024
+
+	idx, err := newIndex(f, c)
+	require.NoError(t, err)
+
+	err = idx.Write(uint32(0), uint64(2))
+	require.NoError(t, err)
+
+	out, pos, err := idx.Read(1)
+	require.Equal(t, uint32(0), out)
+	require.Equal(t, uint64(0), pos)
+	require.Error(t, io.EOF, err)
+}
+
+func TestIndex_returnPositionWhenIndexHasTwoEntries(t *testing.T) {
+	f, err := os.CreateTemp(os.TempDir(), "index_test")
+	require.NoError(t, err)
+	defer func() {
+		if err := os.Remove(f.Name()); err != nil {
+			t.Errorf("got error = %v", err)
+		}
+	}()
+
+	c := Config{}
+	c.Segment.MaxIndexBytes = 1024
+
+	idx, err := newIndex(f, c)
+	require.NoError(t, err)
+
+	err = idx.Write(uint32(0), uint64(2))
+	require.NoError(t, err)
+	err = idx.Write(uint32(1), uint64(999))
+	require.NoError(t, err)
+
+	out, pos, err := idx.Read(1)
+	require.Equal(t, uint32(1), out)
+	require.Equal(t, uint64(999), pos)
+	require.NoError(t, err)
+}
